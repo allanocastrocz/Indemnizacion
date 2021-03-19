cbd919d12219fcee86ac83b7e8f7c716ed4a7b8c
aymeegr <aymee.g.r@hotmail.com>
2021-03-19 11:50:51 -0600

    Actualización BD

--- indemnizacion.sql
+++ indemnizacion.sql
@@ -1,32 +1,203 @@
-CREATE TABLE usuario(
-id INT(11) NOT NULL,
-nombre VARCHAR(10) NOT NULL,
-appat VARCHAR(15) NOT NULL,
-puesto VARCHAR(15) NOT NULL,
-nss VARCHAR(11) NOT NULL,
-salario INT(7,2) NOT NULL,
-status CHAR(1) NULL,
-CONSTRAINT usu_id_pk PRIMARY KEY (id),	
-CONSTRAINT usu_nss_uk UNIQUE (nss));
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-03-2021 a las 18:41:49
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.31
 
-CREATE TABLE motivo(
-id INT(11) NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-CONSTRAINT mot_id_pk PRIMARY KEY (id));
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE derecho(
-id INT(11) NOT NULL,
-derecho VARCHAR(50) NOT NULL,
-motivo INT(11) NOT NULL,
-CONSTRAINT der_id_pk PRIMARY KEY (id),
-CONSTRAINT der_mot_fk FOREIGN KEY (motivo) REFERENCES motivo (id));
 
-CREATE TABLE indemnizacion(
-id INT(11) NOT NULL,
-motivo INT(11) NOT NULL,
-usuario INT(11) NOT NULL,
-CONSTRAINT ind_id_pk PRIMARY KEY (id),
-CONSTRAINT ind_mot_fk FOREIGN KEY (motivo) REFERENCES motivo (id),
-CONSTRAINT ind_usu_fk FOREIGN KEY (usuario) REFERENCES usuario (id));
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Memocha en interfaz gráfica
+--
+-- Base de datos: `indemnizacion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `derecho`
+--
+
+DROP TABLE IF EXISTS `derecho`;
+CREATE TABLE `derecho` (
+  `id` int(11) NOT NULL,
+  `derecho` varchar(50) NOT NULL,
+  `motivo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `derecho`
+--
+
+INSERT INTO `derecho` (`id`, `derecho`, `motivo`) VALUES
+(1, 'Tres meses de salario', 1),
+(2, 'Prima de antiguedad', 1),
+(3, 'Prima vacacional', 1),
+(4, 'Aguinaldo', 1),
+(5, 'Asistencia médica y quirúrgica', 3),
+(6, 'Rehabilitación', 3),
+(7, 'Hospitalización', 3),
+(8, 'Medicamentos y material de curación', 3),
+(9, 'Prótesis', 3),
+(10, 'Parte proporcional de vacaciones', 2),
+(11, 'Parte proporcional de prima vacacional', 2),
+(12, 'Parte proporcional de aguinaldo', 2),
+(13, 'Prima de antiguedad', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `indemnizacion`
+--
+
+DROP TABLE IF EXISTS `indemnizacion`;
+CREATE TABLE `indemnizacion` (
+  `id` int(11) NOT NULL,
+  `motivo` int(11) NOT NULL,
+  `usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `motivo`
+--
+
+DROP TABLE IF EXISTS `motivo`;
+CREATE TABLE `motivo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `motivo`
+--
+
+INSERT INTO `motivo` (`id`, `nombre`) VALUES
+(1, 'Quiebre de empresa'),
+(2, 'Renuncia'),
+(3, 'Incapacidades');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(10) NOT NULL,
+  `appat` varchar(15) NOT NULL,
+  `puesto` varchar(15) NOT NULL,
+  `nss` varchar(11) NOT NULL,
+  `salario` double(7,2) NOT NULL,
+  `status` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `appat`, `puesto`, `nss`, `salario`, `status`) VALUES
+(1, 'Aymee', 'Guarneros', 'Administrador', '45879654123', 2000.00, 'A'),
+(2, 'Brenda', 'Candanedo', 'Administrador', '12987465836', 2000.00, 'A'),
+(3, 'Allan', 'Castro', 'Administrador', '65987432015', 2000.00, 'A'),
+(4, 'Jaime', 'Rojas', 'Empleado', '89745632014', 1200.00, 'A'),
+(5, 'Sonia', 'Viveros', 'Empleado', '32145789632', 1200.00, 'A'),
+(6, 'Juan', 'Murrieta', 'Empleado', '41365740324', 1200.00, 'A'),
+(7, 'Natalia', 'Candanedo', 'Empleado', '36547120387', 1200.00, 'A'),
+(8, 'Yael', 'Guarneros', 'Empleado', '78459632102', 1200.00, 'I'),
+(9, 'Patricia', 'Bustos', 'Empleado', '87456321970', 1200.00, 'I'),
+(10, 'Miguel', 'Palacios', 'Empleado', '97630145863', 1200.00, 'I');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `derecho`
+--
+ALTER TABLE `derecho`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `der_mot_fk` (`motivo`);
+
+--
+-- Indices de la tabla `indemnizacion`
+--
+ALTER TABLE `indemnizacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ind_mot_fk` (`motivo`),
+  ADD KEY `ind_usu_fk` (`usuario`);
+
+--
+-- Indices de la tabla `motivo`
+--
+ALTER TABLE `motivo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usu_nss_uk` (`nss`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `derecho`
+--
+ALTER TABLE `derecho`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `indemnizacion`
+--
+ALTER TABLE `indemnizacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `motivo`
+--
+ALTER TABLE `motivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `derecho`
+--
+ALTER TABLE `derecho`
+  ADD CONSTRAINT `der_mot_fk` FOREIGN KEY (`motivo`) REFERENCES `motivo` (`id`);
+
+--
+-- Filtros para la tabla `indemnizacion`
+--
+ALTER TABLE `indemnizacion`
+  ADD CONSTRAINT `ind_mot_fk` FOREIGN KEY (`motivo`) REFERENCES `motivo` (`id`),
+  ADD CONSTRAINT `ind_usu_fk` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
